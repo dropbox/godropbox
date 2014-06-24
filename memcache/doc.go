553fda57309a5c693666fd22f28818919cd8ac91
Patrick Lee <patrick@dropbox.com>
2014-06-24 04:43:14 +0000

    add package docstring

---
+++ memcache/doc.go
@@ -0,0 +1,7 @@
+// A generic memcache client library which supports connection pooling and
+// flexible sharding.
+//
+// Implementation note: this client uses memcached's binary protocol.  See
+// https://code.google.com/p/memcached/wiki/BinaryProtocolRevamped for
+// additional details.
+package memcache
