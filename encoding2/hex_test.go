707aadb25198f1ec1f4032d133bd4b77b34b9223
Patrick Lee <patrick@dropbox.com>
2014-06-24 23:52:38 +0000

    add encoding2 module

---
+++ encoding2/hex_test.go
@@ -0,0 +1,31 @@
+package encoding2
+
+import (
+    "bytes"
+    "testing"
+
+    . "gopkg.in/check.v1"
+)
+
+func Test(t *testing.T) {
+    TestingT(t)
+}
+
+type HexSuite struct {
+}
+
+var _ = Suite(&HexSuite{})
+
+func (s *HexSuite) TestBasicStreamHex(c *C) {
+    w := bytes.NewBuffer(nil)
+    HexEncodeToWriter(w, []byte("foo"))
+    c.Assert(w.String(), Equals, "666f6f")
+
+    w = bytes.NewBuffer(nil)
+    HexEncodeToWriter(w, []byte(""))
+    c.Assert(w.String(), Equals, "")
+
+    w = bytes.NewBuffer(nil)
+    HexEncodeToWriter(w, []byte("\x00\x01"))
+    c.Assert(w.String(), Equals, "0001")
+}
