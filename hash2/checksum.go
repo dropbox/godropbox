72a3b4d5ebef704e720f09259902559c089b27bf
Patrick Lee <patrick@dropbox.com>
2014-06-26 05:09:30 +0000

    add docstrings

--- hash2/checksum.go
+++ hash2/checksum.go
@@ -5,6 +5,8 @@ import (
 	"crypto/md5"
 )
 
+// This returns the data's MD5 checksum.
+//
 // WARNING: Do NOT Use MD5 in security contexts (defending against
 // intentional manipuations of data from untrusted sources);
 // use only for checking data integrity against machine errors.
@@ -14,6 +16,7 @@ func ComputeMd5Checksum(data []byte) []byte {
 	return h.Sum(nil)
 }
 
+// This returns true iff the data matches the provided checksum.
 func ValidateMd5Checksum(data []byte, sum []byte) bool {
 	ourSum := ComputeMd5Checksum(data)
 	return bytes.Equal(ourSum, sum)
---
+++ hash2/doc.go
@@ -0,0 +1,2 @@
+// This package implements a set of utility hash functions.
+package hash2
