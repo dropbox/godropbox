3533747d2d1592d52890d7c67ee7b0a09a2d5ecb
Patrick Lee <patrick@dropbox.com>
2014-06-26 01:28:31 +0000

    dropbox formatting -> std formatting

--- sync2/boundedrwlock.go
+++ sync2/boundedrwlock.go
@@ -13,58 +13,58 @@
 package sync2
 
 import (
-    "sync"
-    "sync/atomic"
-    "time"
+	"sync"
+	"sync/atomic"
+	"time"
 
-    "github.com/dropbox/godropbox/errors"
+	"github.com/dropbox/godropbox/errors"
 )
 
 // A RWLock that supports timeouts and a capped # of waiting goroutines.
 type BoundedRWLock struct {
-    waiters    chan *rwwait
-    control    *sync.Mutex
-    readers    int
-    nextWriter *rwwait
+	waiters    chan *rwwait
+	control    *sync.Mutex
+	readers    int
+	nextWriter *rwwait
 }
 
 // Create a new BoundedRWLock with the given capacity.
 //
 // RLocks or WLocks beyond this capacity will fail fast with an error.
 func NewBoundedRWLock(capacity int) *BoundedRWLock {
-    return &BoundedRWLock{
-        waiters: make(chan *rwwait, capacity),
-        control: &sync.Mutex{},
-    }
+	return &BoundedRWLock{
+		waiters: make(chan *rwwait, capacity),
+		control: &sync.Mutex{},
+	}
 }
 
 // Wait for a read lock for up to 'timeout'.
 //
 // Error will be non-nil on timeout or when the wait list is at capacity.
 func (rw *BoundedRWLock) RLock(timeout time.Duration) (err error) {
-    deadline := time.After(timeout)
-    rw.control.Lock()
-    if rw.nextWriter != nil {
-        me := newWait(false)
-        select {
-        case rw.waiters <- me:
-        default:
-            err = errors.New("Waiter capacity reached in RLock")
-        }
-        rw.control.Unlock()
-        if err != nil {
-            return
-        }
+	deadline := time.After(timeout)
+	rw.control.Lock()
+	if rw.nextWriter != nil {
+		me := newWait(false)
+		select {
+		case rw.waiters <- me:
+		default:
+			err = errors.New("Waiter capacity reached in RLock")
+		}
+		rw.control.Unlock()
+		if err != nil {
+			return
+		}
 
-        woken := me.WaitAtomic(deadline)
-        if !woken {
-            return errors.New("Waiter timeout")
-        }
-        rw.control.Lock()
-    }
-    rw.readers++
-    rw.control.Unlock()
-    return
+		woken := me.WaitAtomic(deadline)
+		if !woken {
+			return errors.New("Waiter timeout")
+		}
+		rw.control.Lock()
+	}
+	rw.readers++
+	rw.control.Unlock()
+	return
 }
 
 // Unlock a read lock.
@@ -72,48 +72,48 @@ func (rw *BoundedRWLock) RLock(timeout time.Duration) (err error) {
 // Should be called only on a goroutine which has gotten a non-error return
 // value from RLock().
 func (rw *BoundedRWLock) RUnlock() {
-    rw.control.Lock()
-    rw.readers--
-    if rw.readers == 0 {
-        rw.processQueue()
-    }
-    rw.control.Unlock()
+	rw.control.Lock()
+	rw.readers--
+	if rw.readers == 0 {
+		rw.processQueue()
+	}
+	rw.control.Unlock()
 }
 
 // Lock for writing, waiting up to 'timeout' for successful exclusive
 // acquisition of the lock.
 func (rw *BoundedRWLock) WLock(timeout time.Duration) (err error) {
-    deadline := time.After(timeout)
-    rw.control.Lock()
-    if rw.readers != 0 || rw.nextWriter != nil {
-        me := newWait(true)
-        if rw.nextWriter == nil {
-            rw.nextWriter = me
-        } else {
-            select {
-            case rw.waiters <- me:
-            default:
-                err = errors.New("Waiter capacity reached in WLock")
-            }
-        }
-        rw.control.Unlock()
-        if err != nil {
-            return
-        }
+	deadline := time.After(timeout)
+	rw.control.Lock()
+	if rw.readers != 0 || rw.nextWriter != nil {
+		me := newWait(true)
+		if rw.nextWriter == nil {
+			rw.nextWriter = me
+		} else {
+			select {
+			case rw.waiters <- me:
+			default:
+				err = errors.New("Waiter capacity reached in WLock")
+			}
+		}
+		rw.control.Unlock()
+		if err != nil {
+			return
+		}
 
-        woken := me.WaitAtomic(deadline)
-        if !woken {
-            return errors.New("Waiter timeout")
-        }
-        rw.control.Lock()
-        if rw.readers != 0 {
-            panic("readers??")
-        }
-    } else {
-        rw.nextWriter = newWait(true)
-    }
-    rw.control.Unlock()
-    return
+		woken := me.WaitAtomic(deadline)
+		if !woken {
+			return errors.New("Waiter timeout")
+		}
+		rw.control.Lock()
+		if rw.readers != 0 {
+			panic("readers??")
+		}
+	} else {
+		rw.nextWriter = newWait(true)
+	}
+	rw.control.Unlock()
+	return
 }
 
 // Unlock the write lock.
@@ -121,10 +121,10 @@ func (rw *BoundedRWLock) WLock(timeout time.Duration) (err error) {
 // Should be called only on a goroutine which has gotten a non-error return
 // value from WLock().
 func (rw *BoundedRWLock) WUnlock() {
-    rw.control.Lock()
-    rw.nextWriter = nil
-    rw.processQueue()
-    rw.control.Unlock()
+	rw.control.Lock()
+	rw.nextWriter = nil
+	rw.processQueue()
+	rw.control.Unlock()
 }
 
 // Walks the queue of eligible waiters (if any) and wakes them (if they're not
@@ -132,53 +132,53 @@ func (rw *BoundedRWLock) WUnlock() {
 //
 // Any writer "stops" the walk of the queue.
 func (rw *BoundedRWLock) processQueue() {
-    if rw.nextWriter != nil {
-        if rw.nextWriter.WakeAtomic() {
-            return
-        }
-        rw.nextWriter = nil
-    }
+	if rw.nextWriter != nil {
+		if rw.nextWriter.WakeAtomic() {
+			return
+		}
+		rw.nextWriter = nil
+	}
 
-    readersRun := 0
+	readersRun := 0
 
-    for {
-        var next *rwwait
-        select {
-        case next = <-rw.waiters:
-        default:
-            return
-        }
-        if next.writer {
-            // No readers scheduled yet?
-            if readersRun == 0 {
-                // If they wake up, no one else gets to go
-                if next.WakeAtomic() {
-                    rw.nextWriter = next
-                    return
-                }
-            } else {
-                rw.nextWriter = next
-                return
-            }
-        } else {
-            // Reader?  Let them enter now.
-            if next.WakeAtomic() {
-                readersRun++
-            }
-        }
-    }
-    return
+	for {
+		var next *rwwait
+		select {
+		case next = <-rw.waiters:
+		default:
+			return
+		}
+		if next.writer {
+			// No readers scheduled yet?
+			if readersRun == 0 {
+				// If they wake up, no one else gets to go
+				if next.WakeAtomic() {
+					rw.nextWriter = next
+					return
+				}
+			} else {
+				rw.nextWriter = next
+				return
+			}
+		} else {
+			// Reader?  Let them enter now.
+			if next.WakeAtomic() {
+				readersRun++
+			}
+		}
+	}
+	return
 }
 
 // A waiting entity, writer or reader.
 type rwwait struct {
-    writer bool
-    wake   chan bool
-    alive  int32
+	writer bool
+	wake   chan bool
+	alive  int32
 }
 
 func newWait(writer bool) *rwwait {
-    return &rwwait{writer, make(chan bool, 1), 1}
+	return &rwwait{writer, make(chan bool, 1), 1}
 }
 
 // Wait for a signal on the waiter, with the guarantee that both goroutines
@@ -186,18 +186,18 @@ func newWait(writer bool) *rwwait {
 //
 // Returns true if the wake occurred, false on timeout.
 func (wait *rwwait) WaitAtomic(after <-chan time.Time) bool {
-    select {
-    case <-wait.wake:
-        return true
-    case <-after:
-    }
-    swapped := atomic.CompareAndSwapInt32(&wait.alive, 1, 0)
-    // They're gonna put it.
-    if !swapped {
-        <-wait.wake
-        return true
-    }
-    return false
+	select {
+	case <-wait.wake:
+		return true
+	case <-after:
+	}
+	swapped := atomic.CompareAndSwapInt32(&wait.alive, 1, 0)
+	// They're gonna put it.
+	if !swapped {
+		<-wait.wake
+		return true
+	}
+	return false
 }
 
 // Signal the wait to wake.
@@ -205,11 +205,11 @@ func (wait *rwwait) WaitAtomic(after <-chan time.Time) bool {
 // Returns true of the waiter got the signal, false if the waiter timed out
 // before we could deliver the signal.
 func (wait *rwwait) WakeAtomic() bool {
-    swapped := atomic.CompareAndSwapInt32(&wait.alive, 1, 0)
-    if !swapped {
-        // They've moved on.
-        return false
-    }
-    wait.wake <- true
-    return true
+	swapped := atomic.CompareAndSwapInt32(&wait.alive, 1, 0)
+	if !swapped {
+		// They've moved on.
+		return false
+	}
+	wait.wake <- true
+	return true
 }
--- sync2/semaphore_test.go
+++ sync2/semaphore_test.go
@@ -1,14 +1,14 @@
 package sync2
 
 import (
-    "testing"
-    "time"
+	"testing"
+	"time"
 
-    . "gopkg.in/check.v1"
+	. "gopkg.in/check.v1"
 )
 
 func Test(t *testing.T) {
-    TestingT(t)
+	TestingT(t)
 }
 
 type SemaphoreSuite struct {
@@ -17,127 +17,127 @@ type SemaphoreSuite struct {
 var _ = Suite(&SemaphoreSuite{})
 
 func (suite *SemaphoreSuite) TestNonBlockedWait(t *C) {
-    c := make(chan bool)
-    go func() {
-        s := NewSemaphore(3)
-        s.Wait(2)
-        s.Wait(1)
-        s.Increment(1)
-        s.Wait(1)
-        c <- true
-    }()
-    select {
-    case <-c:
-    case <-time.NewTimer(5 * time.Second).C:
-        t.FailNow()
-    }
+	c := make(chan bool)
+	go func() {
+		s := NewSemaphore(3)
+		s.Wait(2)
+		s.Wait(1)
+		s.Increment(1)
+		s.Wait(1)
+		c <- true
+	}()
+	select {
+	case <-c:
+	case <-time.NewTimer(5 * time.Second).C:
+		t.FailNow()
+	}
 }
 
 func (suite *SemaphoreSuite) TestBlockedWait(t *C) {
-    c := make(chan bool)
-    s := NewSemaphore(0)
-    go func() {
-        s.Wait(2)
-        c <- true
-    }()
+	c := make(chan bool)
+	s := NewSemaphore(0)
+	go func() {
+		s.Wait(2)
+		c <- true
+	}()
 
-    s.Increment(1)
+	s.Increment(1)
 
-    select {
-    case <-c:
-        t.FailNow()
-    default:
-    }
+	select {
+	case <-c:
+		t.FailNow()
+	default:
+	}
 
-    s.Increment(1)
+	s.Increment(1)
 
-    select {
-    case <-c:
-    case <-time.NewTimer(5 * time.Second).C:
-        t.FailNow()
-    }
+	select {
+	case <-c:
+	case <-time.NewTimer(5 * time.Second).C:
+		t.FailNow()
+	}
 }
 
 func (suite *SemaphoreSuite) TestMultipleWaiters(t *C) {
-    c := make(chan bool)
-    s := NewSemaphore(0)
-    waiter := func() {
-        s.Wait(1)
-        c <- true
-    }
-    go waiter()
-    go waiter()
+	c := make(chan bool)
+	s := NewSemaphore(0)
+	waiter := func() {
+		s.Wait(1)
+		c <- true
+	}
+	go waiter()
+	go waiter()
 
-    s.Increment(1)
+	s.Increment(1)
 
-    select {
-    case <-c:
-    case <-time.NewTimer(5 * time.Second).C:
-        t.FailNow()
-    }
+	select {
+	case <-c:
+	case <-time.NewTimer(5 * time.Second).C:
+		t.FailNow()
+	}
 
-    s.Increment(1)
+	s.Increment(1)
 
-    select {
-    case <-c:
-    case <-time.NewTimer(5 * time.Second).C:
-        t.FailNow()
-    }
+	select {
+	case <-c:
+	case <-time.NewTimer(5 * time.Second).C:
+		t.FailNow()
+	}
 
-    select {
-    case <-c:
-        t.FailNow()
-    default:
-    }
+	select {
+	case <-c:
+		t.FailNow()
+	default:
+	}
 
-    go waiter()
-    go waiter()
+	go waiter()
+	go waiter()
 
-    select {
-    case <-c:
-        t.FailNow()
-    default:
-    }
-    s.Increment(2)
-    select {
-    case <-c:
-    case <-time.NewTimer(5 * time.Second).C:
-        t.FailNow()
-    }
-    select {
-    case <-c:
-    case <-time.NewTimer(5 * time.Second).C:
-        t.FailNow()
-    }
-    select {
-    case <-c:
-        t.FailNow()
-    default:
-    }
+	select {
+	case <-c:
+		t.FailNow()
+	default:
+	}
+	s.Increment(2)
+	select {
+	case <-c:
+	case <-time.NewTimer(5 * time.Second).C:
+		t.FailNow()
+	}
+	select {
+	case <-c:
+	case <-time.NewTimer(5 * time.Second).C:
+		t.FailNow()
+	}
+	select {
+	case <-c:
+		t.FailNow()
+	default:
+	}
 }
 
 func (suite *SemaphoreSuite) TestLotsOfWaiters(t *C) {
-    c := make(chan bool)
-    s := NewSemaphore(0)
-    waiter := func() {
-        s.Wait(2)
-        c <- true
-    }
-    for i := 0; i < 1000; i++ {
-        go waiter()
-    }
+	c := make(chan bool)
+	s := NewSemaphore(0)
+	waiter := func() {
+		s.Wait(2)
+		c <- true
+	}
+	for i := 0; i < 1000; i++ {
+		go waiter()
+	}
 
-    s.Increment(2000)
-    for found := 0; found < 1000; found++ {
-        <-c
-    }
+	s.Increment(2000)
+	for found := 0; found < 1000; found++ {
+		<-c
+	}
 }
 
 func (suite *SemaphoreSuite) TestWaitWithTimeout(t *C) {
-    s := NewSemaphore(0)
-    res := s.WaitTimeout(1, time.Millisecond)
-    t.Assert(res, Equals, false)
-    s.Increment(1)
-    res = s.WaitTimeout(1, time.Millisecond)
-    t.Assert(res, Equals, true)
+	s := NewSemaphore(0)
+	res := s.WaitTimeout(1, time.Millisecond)
+	t.Assert(res, Equals, false)
+	s.Increment(1)
+	res = s.WaitTimeout(1, time.Millisecond)
+	t.Assert(res, Equals, true)
 }
