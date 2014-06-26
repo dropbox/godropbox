32fd1b6b68e87ee46f6446043084a338a670bc99
Patrick Lee <patrick@dropbox.com>
2014-06-26 05:21:02 +0000

    testing2 -> gocheck

--- hash2/checksum_test.go
+++ hash2/checksum_test.go
@@ -1,27 +1,33 @@
 package hash2
 
 import (
-	"bytes"
 	"testing"
 
-	"dropbox/util/testing2"
+    . "gopkg.in/check.v1"
 )
+
+func Test(t *testing.T) {
+    TestingT(t)
+}
+
+type ChecksumSuite struct {
+}
+
+var _ = Suite(&ChecksumSuite{})
 
 var input = []byte("it just works")
 var md5Reference = []byte("qp\xbdZ\x9dSH\xe81\x10\x0fk\x81\xff\xda\xdc")
 
-func TestHash(t *testing.T) {
-	h := testing2.H{t}
-
+func (s *ChecksumSuite) TestHash(c *C) {
 	ourMd5 := ComputeMd5Checksum(input)
-	h.Assert(bytes.Equal(ourMd5, md5Reference), "md5s match")
+	c.Assert(ourMd5, DeepEquals, md5Reference)
 
-	h.Assert(ValidateMd5Checksum(input, md5Reference), "md5 validates")
-	h.Assert(ValidateMd5Checksum(input, ourMd5), "md5 validates")
+	c.Assert(ValidateMd5Checksum(input, md5Reference), Equals, true)
+	c.Assert(ValidateMd5Checksum(input, ourMd5), Equals, true)
 
 	// Fails...
-	h.Assert(!ValidateMd5Checksum(ourMd5, input), "reversal doesn't work")
+	c.Assert(ValidateMd5Checksum(ourMd5, input), Equals, false)
 
 	// No panic on empty
-	h.Assert(!ValidateMd5Checksum([]byte(""), []byte("")), "wrong sum is wrong")
+	c.Assert(ValidateMd5Checksum([]byte(""), []byte("")), Equals, false)
 }
