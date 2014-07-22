584c9920f20fa80501758f91a204e69265e24981
Rajat Goel <rajat.goel2010@gmail.com>
2014-07-22 10:59:27 -0700

    Add BytesEqual to gocheck2

--- gocheck2/checkers.go
+++ gocheck2/checkers.go
@@ -5,6 +5,8 @@
 package gocheck2
 
 import (
+    "bytes"
+
 	. "gopkg.in/check.v1"
 )
 
@@ -51,3 +53,34 @@ var IsFalse Checker = &isBoolValueChecker{
 	&CheckerInfo{Name: "IsFalse", Params: []string{"obtained"}},
 	false,
 }
+
+// -----------------------------------------------------------------------
+// BytesEqual checker compares two bytes sequence using bytes.Equal
+
+type bytesEquals struct{}
+
+func (b *bytesEquals) Check(params []interface{}, names []string) (bool, string) {
+    if len(params) != 2 {
+        return false, "BytesEqual takes 2 bytestring arguments"
+    }
+    b1, ok1 := params[0].([]byte)
+    b2, ok2 := params[1].([]byte)
+
+    if !(ok1 && ok2) {
+        return false, "Arguments to BytesEqual must both be bytestrings"
+    }
+
+    if bytes.Equal(b1, b2) {
+        return true, ""
+    }
+    return false, "Byte arrays were different"
+}
+
+func (b *bytesEquals) Info() *CheckerInfo {
+    return &CheckerInfo{
+        Name:   "BytesEquals",
+        Params: []string{"bytes_one", "bytes_two"},
+    }
+}
+
+var BytesEquals = &bytesEquals{}
